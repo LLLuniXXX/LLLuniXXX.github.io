a301288066e84ca64ac3aba6320a13175eb8cf80
llunixx <138971080+LLLuniXXX@users.noreply.github.com>
2026-02-28 13:44:49 +0530

    Add files via upload

---
+++ client.py
@@ -0,0 +1,38 @@
+import socket
+import cv2
+import pickle
+import struct
+
+HOST = "192.168.29.79"
+PORT = 9999
+
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect((HOST, PORT))
+
+data = b""
+payload_size = struct.calcsize("Q")
+
+while True:
+    while len(data) < payload_size:
+        packet = client_socket.recv(4096)
+        if not packet:
+            break
+        data += packet
+
+    packed_msg_size = data[:payload_size]
+    data = data[payload_size:]
+    msg_size = struct.unpack("Q", packed_msg_size)[0]
+
+    while len(data) < msg_size:
+        data += client_socket.recv(4096)
+
+    frame_data = data[:msg_size]
+    data = data[msg_size:]
+
+    frame = pickle.loads(frame_data)
+    cv2.imshow("Live Camera Feed", frame)
+
+    if cv2.waitKey(1) == ord("q"):
+        break
+
+client_socket.close()
